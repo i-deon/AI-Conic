2c8be96d1d7613037a596e3b3729ee172c8535d6
sohyeon <studyhyeon1004@naver.com>
2025-11-23 16:00:15 +0900

    feat(환경설정) : intelliJ 기본 환경 설정

---
+++ i_deon/src/test/java/com/i_deon/IDeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i_deon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
