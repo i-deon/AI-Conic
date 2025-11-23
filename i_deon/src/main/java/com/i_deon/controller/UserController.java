e856a24834f40520c9df4e4d059e63571b6bbc96
sohyeon <studyhyeon1004@naver.com>
2025-11-23 16:39:33 +0900

    refactor(user) : swagger 설정 추가

--- i_deon/src/main/java/com/i_deon/controller/UserController.java
+++ i_deon/src/main/java/com/i_deon/controller/UserController.java
@@ -3,6 +3,8 @@ package com.i_deon.controller;
 import com.i_deon.dto.response.GuardianCodexResponse;
 import com.i_deon.dto.response.UserProfileResponse;
 import com.i_deon.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,14 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "User API", description = "유저 관련 API 모음")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "내 프로필 확인")
     @GetMapping("/me")
-    public UserProfileResponse getMyProfile(
-            @RequestHeader("X-USER-ID") Long userId
-    ) {
+    public UserProfileResponse getMyProfile(@RequestHeader("X-USER-ID") Long userId) {
         return userService.getMyProfile(userId);
     }
 
